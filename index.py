c71b6b1db79355e2ac0956ab8cabd5844962b182
pdeesawat4887 <31108772+pdeesawat4887@users.noreply.github.com>
2018-07-12 17:07:30 +0700

    prototype v0.0.0.1

--- index.py
+++ index.py
@@ -1,7 +1,19 @@
-#!/usr/bin/env python
+#!/Applications/XAMPP/xamppfiles/htdocs/python/venv/bin/python
 import cgi
-
 from easysnmp import Session
+
+class Router:
+
+    def __init__(self, host, community, version):
+        self.host = host
+        self.community = community
+        self.version = version
+        self.session =  Session(hostname=self.host,
+                               community=self.community, version=self.version)
+
+    def walkthrough(self, oid):
+        items = self.session.walk(oid)
+        return items
 
 print "Content-type: text/html\n\n"
 print '<!DOCTYPE html>'
@@ -25,9 +37,11 @@ print 'My first website with <strong>Bulma</strong>!'
 print '</p>'
 print '<br><br>'
 print '<form method="POST" action="index.py">'
-print '<p>Fill ip address of router that you want to monitor.</p><br>'
+print '<p class="subtitle">Fill ip address of router that you want to monitor.</p>'
 print '<p><input name="ipaddress" class="input is-rounded" type="text" placeholder="ex. 192.168.10.1"/></p><br>'
-print '<p><input type="submit" class="button is-primary" values="submit" />'
+print '<p class="subtitle">Fill oid in MIB-II that you want to monitor.</p>'
+print '<p><input name="oid" class="input is-rounded" type="text" placeholder="system or 1.6.3.9.1"/></p><br>'
+print '<p><input type="submit" class="button is-primary" value="submit" />'
 print '</form>'
 
 
@@ -35,22 +49,15 @@ form = cgi.FieldStorage()
 if form.getvalue("ipaddress"):
     ipaddress = form.getvalue('ipaddress')
     print '<br><h1> You want to monitor router that contain ip address <strong>'+ipaddress+'.</strong> Wait a min ... </h1><br>'
+if form.getvalue("oid"):
+    oid = form.getvalue('oid')
 
+router = Router(ipaddress, 'public', 2)
+info = router.walkthrough(oid)
+help = '-'
+for item in info:
+    print '<p> {:.<50} <strong> {} </strong>'.format(item.oid, item.value)
 print '</div>'
 print '</section>'
 print '</body>'
 print '</html>'
-
-
-
-class Router:
-
-    def __init__(self, host, community, version):
-        self.host = host
-        self.community = community
-        self.version = version
-        self.session =  Session(hostname=self.host,
-                               community=self.community, version=self.version)
-
-    # def walkthrough(self):
-    #     pass
