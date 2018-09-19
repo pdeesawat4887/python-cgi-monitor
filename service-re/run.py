a288bcba156c9f6f93b0b77fcc5016619408d2be
pdeesawat4887 <31108772+pdeesawat4887@users.noreply.github.com>
2018-09-19 13:55:02 +0700

    update release3

--- service-re/run.py
+++ service-re/run.py
@@ -1,7 +1,9 @@
 import main.probe as probe
+import os
 
 if __name__ == '__main__':
-    probeMac = probe.Probe()
+    path = os.path.dirname(os.path.abspath(__file__))
+    probeMac = probe.Probe(path)
     print probeMac.ip
     print probeMac.mac_address
     print probeMac.id
